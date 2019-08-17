ccbf708e802dcb3886eb06403f6575cc7dcdf939
Twinkle vaid <twinkle@tothenew.com>
2019-08-17 18:32:38 +0530

    promise example

---
+++ app.js
@@ -0,0 +1,14 @@
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+.then((data)=> {
+    console.log("Api Data", data)
+    fetch('https://jsonplaceholder.typicode.com/todos/1/users')
+    .then((users) => {
+        console.log("fetch users", users)
+    })
+})
+.catch((err)=> {
+    console.log("rejected", err)
+})
+.finally(()=> {
+    console.log("finally called")
+})
